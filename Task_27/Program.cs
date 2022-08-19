f5c8a3c38ff6be1ce6101505452dcfce0b4dc44d
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-19 06:00:00 +0400

    Task_27 added

---
+++ Task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetSum (int number)
+{
+    int sum = 0;
+    
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Enter a number: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+num = Math.Abs(num);
+Console.WriteLine($"Sum of the digits in entered number = {GetSum(num)}");
